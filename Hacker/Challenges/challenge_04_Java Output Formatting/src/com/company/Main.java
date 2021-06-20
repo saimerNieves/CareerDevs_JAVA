bae27453ecd7a35227aee951d65f193b9b867b72
Saimer <saimer.nieves@gmail.com>
2021-06-20 15:43:51 -0400

    Completed challenge 4 Java Output Formatting

--- Hacker/Challenges/challenge_04_Java Output Formatting/src/com/company/Main.java
+++ Hacker/Challenges/challenge_04_Java Output Formatting/src/com/company/Main.java
@@ -4,18 +4,35 @@ import java.util.Scanner;
 
 public class Main {
 
+
+
+    public static String sizedString(String string_input, int size_input){
+
+        String cleanedStr = String.format("%1$" + size_input + "s", string_input);
+
+
+        return cleanedStr;
+    }
+
     public static void main(String[] args) {
 	// write your code here
-
-        Scanner sc=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("================================");
-        for(int i=0;i<3;i++)
+        for(int i = 0; i < 3; i++)
         {
-            String s1=sc.next();
-            int x=sc.nextInt();
-            //Complete this line
+            String s1 = scan.next();
+            int x = scan.nextInt();
+
+            int y = scan.nextInt();
+            //% - minus symbol means left justified
+            // 15 width
+            //.25 precision of max
+
+            System.out.printf("%-15s%03d%9d",s1,x,y);
         }
         System.out.println("================================");
+
+//        System.out.println(sizedString("java",15));
 
     }
 }
