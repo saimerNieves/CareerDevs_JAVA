14a9ad63c21086b7c15d4a95baf708d64e8a2183
Saimer <saimer.nieves@gmail.com>
2021-06-19 23:33:43 -0400

    Began challenge 05 Java Loops 1

---
+++ Hacker/Challenges/challenge_05_Java Loops 1/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //Saimer Nievs continue here
+
+        System.out.println("JAVA Loops 1 Site continue in the morning");
+    }
+}
