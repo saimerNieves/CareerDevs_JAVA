0903d1979d0e5478d8da8459ba622590d778a53c
saimer-nieves <snieves@email.neit.edu>
2021-06-22 09:49:55 -0400

    Created an Array list inside hacker 7 for testing purposes

--- Hacker/hacker_07/src/com/company/Main.java
+++ Hacker/hacker_07/src/com/company/Main.java
@@ -1,10 +1,21 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
         System.out.println("This is where I will do my Hacker 07");
+
+        ArrayList<String> namesList = new ArrayList<>();
+
+        namesList.add("Saimer");
+        namesList.add("Nieves");
+
+        for (String name : namesList){
+            System.out.println(name);
+        }
     }
 }
