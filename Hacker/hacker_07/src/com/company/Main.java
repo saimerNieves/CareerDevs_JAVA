6f75c3bc429b9d924015ddb357ff069f370fde65
Saimer <saimer.nieves@gmail.com>
2021-06-22 18:23:59 -0400

    Completed Hacker 07 Challenge

---
+++ Hacker/hacker_07/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+	// write your code here
+
+        BufferedReader buff = new BufferedReader( new InputStreamReader(System.in));
+        int arraySize = Integer.parseInt( buff.readLine().trim());
+
+        String[] numArray = new String[arraySize];
+
+
+        String all_numbers_input = buff.readLine().trim();
+        numArray = all_numbers_input.split(" ");
+
+
+        for (int i = arraySize-1; i >= 0; i--){
+            System.out.print(numArray[i] + " ");
+        }
+
+
+
+
+        buff.close();
+    }
+}
