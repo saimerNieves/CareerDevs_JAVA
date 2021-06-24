901dbae04a5d7cc477c1cd046fc49358075faa5d
Saimer <saimer.nieves@gmail.com>
2021-06-23 23:21:25 -0400

    Created a HashMap

--- Hacker/hacker_08/src/com/company/Main.java
+++ Hacker/hacker_08/src/com/company/Main.java
@@ -1,8 +1,60 @@
 package com.company;
 
+import com.sun.jdi.Value;
+
+import javax.swing.plaf.basic.BasicButtonUI;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
+
+        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+
+        Map<String,String> phoneNumberDictionary = new HashMap<>();
+
+        int input_length = Integer.parseInt( buff.readLine().trim());
+        for (int i = 0 ; i < input_length; i++){
+
+            String[] key_value_Inputed = buff.readLine().trim().split(" ");
+            String name_Key = key_value_Inputed[0];
+            String phoneNumber_Value = key_value_Inputed[1];
+
+            phoneNumberDictionary.put(name_Key,phoneNumber_Value);
+        }
+
+        String keyArray =  phoneNumberDictionary.keySet().toString();
+
+        String valueArray = phoneNumberDictionary.values().toString();
+
+        System.out.print(keyArray + " ");
+        System.out.println();
+
+
+
+
+        buff.close();
+        //Phone number map
+//
+//        Map<String, String> phoneNumberMap = new HashMap<>();
+//        phoneNumberMap.put("Saimer","401-999-9999");
+//        phoneNumberMap.put("Luis","498-888-5415");
+//
+//        System.out.println( phoneNumberMap.get("Saimer"));
+//
+//        System.out.println( phoneNumberMap.keySet());
+//
+//        System.out.println(phoneNumberMap.values());
+//
+//        System.out.println(phoneNumberMap.size());
+
     }
 }
