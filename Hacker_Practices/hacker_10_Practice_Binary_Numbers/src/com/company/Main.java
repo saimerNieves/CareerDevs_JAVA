344aa64358c91f244dd5284fc776efa32bcc5894
Saimer <saimer.nieves@gmail.com>
2021-06-25 23:20:34 -0400

    Completed HackerRank Day 10

--- Hacker_Practices/hacker_10_Practice_Binary_Numbers/src/com/company/Main.java
+++ Hacker_Practices/hacker_10_Practice_Binary_Numbers/src/com/company/Main.java
@@ -1,11 +1,28 @@
 package com.company;
 
+
+import java.util.Scanner;
+
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        System.out.println(consecutiveOnes(n));
+    }
 
-        //Binary Numbers 10101001110
-
+    public static int consecutiveOnes(int n) {
+        int count = 0;
+        int max   = 0;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+            n = n >> 1;
+        }
+        return max;
     }
 }
