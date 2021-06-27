36b8c5ef9f8e0d5aac8c524062813299005486bb
Saimer <saimer.nieves@gmail.com>
2021-06-26 21:57:08 -0400

    Completed hacker 10 and foudn highest sequence of binary number 1s

--- Hacker/hacker_10/src/com/company/Main.java
+++ Hacker/hacker_10/src/com/company/Main.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     //5
@@ -18,13 +22,67 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
 
+        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
 
+        int inputBuffNum = Integer.parseInt( buff.readLine().trim());
 
-        System.out.println( getBinary(45));
+        String binaryNumber =  getBinary(inputBuffNum);
 
+        String[] binarySplit_array = binaryNumber.split("");
+
+        int counter = 0;
+        int highestNum = 0;
+        for (int i = 0; i < binarySplit_array.length; i++)
+        {
+            if(binarySplit_array[i].equals("1")){
+                counter++;
+                if(counter > highestNum){
+                    highestNum = counter;
+                }
+
+            }
+            else if( binarySplit_array[i].equals("0") && (counter > 0)){
+                counter --;
+            }
+
+        }
+
+        System.out.println("Counter: " + counter);
+        System.out.println("highest Number: " + highestNum);
+
+        buff.close();
 
     }
 }
+
+
+/*
+* import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        System.out.println(consecutiveOnes(n));
+    }
+
+    public static int consecutiveOnes(int n) {
+        int count = 0;
+        int max   = 0;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+            n = n >> 1;
+        }
+        return max;
+    }
+}
+*/
