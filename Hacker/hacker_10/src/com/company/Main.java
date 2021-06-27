5212461ba4fb3e871309c1ad77936b062bea9cfd
Saimer <saimer.nieves@gmail.com>
2021-06-26 21:26:55 -0400

    Recreated Hacker_10 using Recursions

--- Hacker/hacker_10/src/com/company/Main.java
+++ Hacker/hacker_10/src/com/company/Main.java
@@ -6,37 +6,24 @@ public class Main {
     public static String getBinary(int newNumber){
 
         //Base Case
-        if((newNumber/2) < 2){
-            System.out.println("Ran here first");
-            System.out.println(String.valueOf (newNumber % 2));
-            return  String.valueOf (newNumber % 2); // 101
-
-
+        if(newNumber < 2){
+            return  String.valueOf (newNumber ); // 101
         }
-
         //Recursive Case
         else{
-            System.out.println("Ran here second");
+
             int binaryNum = (newNumber % 2); // 1 // 0
             int quotient = (int) Math.floor(newNumber/2); // 2 //  1
-            System.out.println(binaryNum);
-            return String.valueOf(binaryNum) + getBinary(quotient);// 1 // 0
-
-
-
+            return   getBinary(quotient) + String.valueOf(binaryNum);// 1 // 0
         }
-
-
     }
 
     public static void main(String[] args) {
 	// write your code here
 
-        String Saimer = "";
-
-      getBinary(21);
 
 
+        System.out.println( getBinary(45));
 
 
     }
