582406572871d35a09a81c81723ed5b5b8fb532f
Saimer <saimer.nieves@gmail.com>
2021-06-28 21:15:06 -0400

    Completed Main Class in Hacker 14

--- Hacker_Practices/hacker_14_Practice_Scope/src/com/company/Main.java
+++ Hacker_Practices/hacker_14_Practice_Scope/src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
     }
 }
