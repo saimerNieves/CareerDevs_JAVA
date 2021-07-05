4783cc60565e7f0cdee7a324689f2374e312d3e2
Saimer <saimer.nieves@gmail.com>
2021-07-04 22:23:57 -0400

    Completed Hacker 19

---
+++ Hacker_Practices/hacker_19_Interfaces/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+
+        AdvancedArithmetic myCalculator = new Calculator();
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
