166512808e6fec3cfe797dde0242c467e8e3779a
Saimer <saimer.nieves@gmail.com>
2021-07-06 22:08:45 -0400

    Created Scanner Input

--- Hacker_Practices/hacker_21_generics/src/com/company/Main.java
+++ Hacker_Practices/hacker_21_generics/src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
     }
 }
