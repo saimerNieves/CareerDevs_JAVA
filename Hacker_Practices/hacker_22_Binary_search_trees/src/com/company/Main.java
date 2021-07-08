44b1ffb17d1c0235b93451dfc7866d99176a7739
Saimer <saimer.nieves@gmail.com>
2021-07-07 22:36:57 -0400

    Created Static Methods

--- Hacker_Practices/hacker_22_Binary_search_trees/src/com/company/Main.java
+++ Hacker_Practices/hacker_22_Binary_search_trees/src/com/company/Main.java
@@ -2,6 +2,30 @@ package com.company;
 
 public class Main {
 
+    public static int getHeight(Node root) {
+        if (root == null) {
+            return -1;
+        } else {
+            return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+        }
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
     public static void main(String[] args) {
 	// write your code here
     }
