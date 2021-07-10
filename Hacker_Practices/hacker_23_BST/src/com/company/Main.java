3065002a0158204515fea626c82e0b168e9e6f75
Saimer <saimer.nieves@gmail.com>
2021-07-09 22:32:16 -0400

    Finished hacker 23 BST

--- Hacker_Practices/hacker_23_BST/src/com/company/Main.java
+++ Hacker_Practices/hacker_23_BST/src/com/company/Main.java
@@ -41,6 +41,13 @@ public class Main {
     }
 
     public static void main(String args[]) {
-
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        Node root = null;
+        while (T-- > 0) {
+            int data = sc.nextInt();
+            root = insert(root, data);
+        }
+        levelOrder(root);
     }
 }
