e99a28afba665aedd4af6159a53b9cfd5c51c77d
Saimer <saimer.nieves@gmail.com>
2021-07-10 23:45:58 -0400

    Edited the dispenser class

--- DrinkDispenser_V3/src/com/company/DrinkDispenser.java
+++ DrinkDispenser_V3/src/com/company/DrinkDispenser.java
@@ -1,4 +1,23 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class DrinkDispenser {
+
+    public ArrayList<Syrup> syrups; // Collection of multiple instances of Syrup Class.
+
+    //All new Cupholders
+    public Cupholder small;
+    public Cupholder medium;
+    public Cupholder large;
+
+
+    //Contructors => If the constructor is called for the same instance again it will restart the arrary list thats why is initialized in the constructor
+    public DrinkDispenser(){
+        //syrups = new ArrayList<>();
+    }
+
+
+
+
 }
