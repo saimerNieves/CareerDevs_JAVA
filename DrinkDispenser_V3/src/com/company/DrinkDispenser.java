0b6c1766060a1005ada6c275beb9fb5f5d0f358a
Saimer <saimer.nieves@gmail.com>
2021-07-11 13:42:10 -0400

    Dispense one Drink and tested Cups and Syrups methods

--- DrinkDispenser_V3/src/com/company/DrinkDispenser.java
+++ DrinkDispenser_V3/src/com/company/DrinkDispenser.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class DrinkDispenser {
 
-    public ArrayList<Syrup> syrups; // Collection of multiple instances of Syrup Class.
+    public ArrayList<Syrup> syrups_Array; // Collection of multiple instances of Syrup Class.
 
     //All new Cupholders
     public Cupholder small;
@@ -14,7 +14,7 @@ public class DrinkDispenser {
 
     //Constructors => If the constructor is called for the same instance again it will restart the arrary list thats why is initialized in the constructor
     public DrinkDispenser(int small_vol_int, int medium_vol_int, int large_vol_int){
-        this.syrups = new ArrayList<>();
+        this.syrups_Array = new ArrayList<>();
 
         this.small = new Cupholder(small_vol_int);   // Calls the CupHolder Constructor and provides the small int stating volume size in ounces
         this.medium = new Cupholder(medium_vol_int); // Calls the CupHolder Constructor and provides the medium int stating volume size in ounces
@@ -41,17 +41,21 @@ public class DrinkDispenser {
                 System.out.println("ERROR Invalid size");
                 return;
         }
-         syrups.get(syrup_index).dispense(volume); //After the cup is dispensed in your hand the syrup ArrayList will get called with an index and it will dispense the volume value return from the cup
+         String drinkName = syrups_Array.get(syrup_index).dispense(volume); //After the cup is dispensed in your hand the syrup ArrayList will get called with an index and it will dispense the volume value return from the cup
+
+        //Output should be size and drink
+        System.out.printf("%s %s\n\n", size_str, drinkName);
+
     }
 
     //Add syrup to arrayList after is all reade created
     public void addSyrup(Syrup newSyrup){
-        syrups.add(newSyrup);
+        syrups_Array.add(newSyrup);
     }
 
     //Adding a new syrup just by name and volume
     public void addSyrup(String name, int volume){
-        syrups.add(new Syrup(name, volume)); // This is building using a constructor that takes the name and volume of drink
+        syrups_Array.add(new Syrup(name, volume)); // This is building using a constructor that takes the name and volume of drink
     }
 
 
--- DrinkDispenser_V3/src/com/company/Main.java
+++ DrinkDispenser_V3/src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
 
 
@@ -12,6 +14,8 @@ public class Main {
 
         DrinkDispenser mcDonalds_Drinks = new DrinkDispenser(5,8,12);
 
+        mcDonalds_Drinks.medium.reFill(20);
+
         //Adding syrup indide of the DrinkDispenser
 
         mcDonalds_Drinks.addSyrup(new Syrup("Dr Pepper",50));
@@ -19,8 +23,17 @@ public class Main {
         mcDonalds_Drinks.addSyrup(new Syrup("Sprite",40));
         mcDonalds_Drinks.addSyrup("Grape Soda",70);
 
+
+        System.out.printf("Cups Amount: %d\t   Syrup Volume Left: %f\n", mcDonalds_Drinks.medium.quantity ,  mcDonalds_Drinks.syrups_Array.get(2).getVolume());
+
+
+
+        //This is serving the cup down and is also inserting the syrup juice
+        //from array list with index 2
         mcDonalds_Drinks.serveDrink("medium",2);
 
+
+        System.out.printf("Cups Amount: %d\t   Syrup Volume Left: %f\n", mcDonalds_Drinks.medium.quantity ,  mcDonalds_Drinks.syrups_Array.get(2).getVolume());
 
 
     }
