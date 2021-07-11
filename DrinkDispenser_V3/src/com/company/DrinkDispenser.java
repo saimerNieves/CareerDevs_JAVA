0a89ae7b8d5e75fd77185eb1a0f74b4c4821723d
Saimer <saimer.nieves@gmail.com>
2021-07-11 11:21:14 -0400

    Created a new Instance of McDonalds Drink dispenser

--- DrinkDispenser_V3/src/com/company/DrinkDispenser.java
+++ DrinkDispenser_V3/src/com/company/DrinkDispenser.java
@@ -12,14 +12,49 @@ public class DrinkDispenser {
     public Cupholder large;
 
 
-    //Contructors => If the constructor is called for the same instance again it will restart the arrary list thats why is initialized in the constructor
-    public DrinkDispenser(int small_int, int medium_int, int large_int){
-        //syrups = new ArrayList<>();
+    //Constructors => If the constructor is called for the same instance again it will restart the arrary list thats why is initialized in the constructor
+    public DrinkDispenser(int small_vol_int, int medium_vol_int, int large_vol_int){
+        this.syrups = new ArrayList<>();
 
-        this.small = new Cupholder(small_int);   // Calls the CupHolder Constructor and provides the small int stating volume size in ounces
-        this.medium = new Cupholder(medium_int); // Calls the CupHolder Constructor and provides the medium int stating volume size in ounces
-        this.large = new Cupholder(large_int);   // Calls the CupHolder Constructor and provides the large int stating volume size in ounces
+        this.small = new Cupholder(small_vol_int);   // Calls the CupHolder Constructor and provides the small int stating volume size in ounces
+        this.medium = new Cupholder(medium_vol_int); // Calls the CupHolder Constructor and provides the medium int stating volume size in ounces
+        this.large = new Cupholder(large_vol_int);   // Calls the CupHolder Constructor and provides the large int stating volume size in ounces
     }
+
+    public void serveDrink(String size_str,int syrup_index){
+
+        int volume = 0;
+        switch(size_str.toLowerCase()){
+            case "small":
+                volume = small.dispenseCup();
+                break;
+
+            case "medium":
+                volume = medium.dispenseCup();
+                break;
+
+            case "large":
+                volume = large.dispenseCup();
+                break;
+
+            default:
+                System.out.println("ERROR Invalid size");
+                return;
+        }
+         syrups.get(syrup_index).dispense(volume); //After the cup is dispensed in your hand the syrup ArrayList will get called with an index and it will dispense the volume value return from the cup
+    }
+
+    //Add syrup to arrayList after is all reade created
+    public void addSyrup(Syrup newSyrup){
+        syrups.add(newSyrup);
+    }
+
+    //Adding a new syrup just by name and volume
+    public void addSyrup(String name, int volume){
+        syrups.add(new Syrup(name, volume)); // This is building using a constructor that takes the name and volume of drink
+    }
+
+
 
 
 
--- DrinkDispenser_V3/src/com/company/Main.java
+++ DrinkDispenser_V3/src/com/company/Main.java
@@ -4,5 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
+        //At the factory we are making this dispenser
+        //We passed down 3 sizes so that it will know what type of cupholders will be built in
+        //Right now is just a DrinkDispenser with built in Cupholders
+
+        DrinkDispenser mcDonalds_Drinks = new DrinkDispenser(5,8,12);
+
+        //Adding syrup indide of the DrinkDispenser
+
+        mcDonalds_Drinks.addSyrup(new Syrup("Dr Pepper",50));
+        mcDonalds_Drinks.addSyrup("Coke",20);
+        mcDonalds_Drinks.addSyrup(new Syrup("Sprite",40));
+        mcDonalds_Drinks.addSyrup("Grape Soda",70);
+
+        mcDonalds_Drinks.serveDrink("medium",2);
+
+
+
     }
 }
