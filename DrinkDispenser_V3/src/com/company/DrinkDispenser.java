dc905882938593e3e434e59fc1fa8d23d0640336
Saimer <saimer.nieves@gmail.com>
2021-07-11 00:06:34 -0400

    Edited DrinkDispenser Constructor sizes

--- DrinkDispenser_V3/src/com/company/DrinkDispenser.java
+++ DrinkDispenser_V3/src/com/company/DrinkDispenser.java
@@ -13,8 +13,12 @@ public class DrinkDispenser {
 
 
     //Contructors => If the constructor is called for the same instance again it will restart the arrary list thats why is initialized in the constructor
-    public DrinkDispenser(){
+    public DrinkDispenser(int small_int, int medium_int, int large_int){
         //syrups = new ArrayList<>();
+
+        this.small = new Cupholder(small_int);   // Calls the CupHolder Constructor and provides the small int stating volume size in ounces
+        this.medium = new Cupholder(medium_int); // Calls the CupHolder Constructor and provides the medium int stating volume size in ounces
+        this.large = new Cupholder(large_int);   // Calls the CupHolder Constructor and provides the large int stating volume size in ounces
     }
 
 
