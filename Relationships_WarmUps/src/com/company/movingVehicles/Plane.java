d06a718654a8ce7d3dbc7bd880e0b379c0c3b99a
Saimer <saimer.nieves@gmail.com>
2021-07-12 21:55:17 -0400

    made adjustments

--- Relationships_WarmUps/src/com/company/movingVehicles/Plane.java
+++ Relationships_WarmUps/src/com/company/movingVehicles/Plane.java
@@ -2,7 +2,6 @@ package com.company.movingVehicles;
 
 import com.company.*;
 import com.company.vehicle_attriubute.*;
-
 import java.util.ArrayList;
 
 
@@ -11,38 +10,37 @@ public class Plane extends Vehicle {
     //fields
 
     //TODO make sure to take away these fields because they are already inhertied
-    public String type = "Plane";
-    public boolean isLand = false;
-    public boolean isWater = false;
-    public boolean isAir = true;
-    public boolean isSpace = false;
-    public Passenger passengers;
-    public ArrayList<Tire> tires;
+    public ArrayList<Tire> tires = new ArrayList<>();
     public boolean isLanding;
 
 
     //Constructor
-
     public Plane(Engine plane_engine, ArrayList<Tire> tires, int vehicle_maxPassenger ){
 
         this.engine = plane_engine;
         this.maxPassengers = vehicle_maxPassenger;
         this.tires = tires;
 
+        //Default parent setting fields
+        this.type = "Plane";
+        this.isLand = false;
+        this.isWater = false;
+        this.isAir = true;
+        this.isSpace = false;
     }
+
 
     //Methods
 
-
     public void toggleLanding(){
+
         this.isLanding = true;
     }
 
     public void addTire(Tire newTire){
+
         if(this.tires.size() < 6){
-
             tires.add(newTire);
-
         }
         else{
             System.out.println("too many tires");
@@ -52,20 +50,18 @@ public class Plane extends Vehicle {
     public void removeTire( Tire oldtire){
 
         if(this.tires.size() > 0){
-
             tires.remove(oldtire);
-
         }
         else{
             System.out.println("No more tires");
         }
     }
 
-    public void replaceTire(){}
 
 
+    //TODO this is a bonus make sure you complete it
+    public void replaceTire(){
 
-
-
+    }
 
 }
