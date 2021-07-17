d166aac6202a7e71c88f920462ded8d57c09b6f8
Saimer <saimer.nieves@gmail.com>
2021-07-16 22:41:38 -0400

    Updated instructions

--- Program Flow with High Low/src/com/company/Main.java
+++ Program Flow with High Low/src/com/company/Main.java
@@ -2,7 +2,8 @@ package com.company;
 
 public class Main {
 
-    //Todo watch the class video for high class to make game
+    //Todo watch the class video for high class to make
+    //Todo  changed the high low game from a single round to a game that repeats until you lose.
     public static void main(String[] args) {
 	// write your code here
     }
