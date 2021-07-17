7875aed5fba7cf180b01e88dad86dcb070d42325
Saimer <saimer.nieves@gmail.com>
2021-07-16 22:41:10 -0400

    Updated instructions

--- Program Flow with High Low/src/com/company/Main.java
+++ Program Flow with High Low/src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class Main {
 
+    //Todo watch the class video for high class to make game
     public static void main(String[] args) {
 	// write your code here
     }
