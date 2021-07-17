7cdf3e2fa95df17f77e006214f14dc0bc8528112
Saimer <saimer.nieves@gmail.com>
2021-07-16 22:42:12 -0400

    Created template to code

--- Program Flow with High Low/src/com/company/Main.java
+++ Program Flow with High Low/src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;
 
+//Saimer Nieves 7-16-2021
 public class Main {
 
     //Todo watch the class video for high class to make
     //Todo  changed the high low game from a single round to a game that repeats until you lose.
+
     public static void main(String[] args) {
 	// write your code here
     }
